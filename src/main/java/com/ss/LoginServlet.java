789656f8d28a739934ea9b28611c7e7507d1a889
StefanStoynov <sstoynov87@gmail.com>
2022-01-16 19:06:11 +0200

    Introduce welcome JSP and doPost method

--- src/main/java/com/ss/LoginServlet.java
+++ src/main/java/com/ss/LoginServlet.java
@@ -12,13 +12,16 @@ import java.io.PrintWriter;
 public class LoginServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // url: http://localhost:8080/?username=Stefan&password=123
-        //how to pass the value to the JSP - use attributes
-        request.setAttribute("username", request.getParameter("username"));
-        request.setAttribute("password", request.getParameter("password"));
         //redirecting to JSP
         //Example: request.getRequestDispatcher("Where my JSP is").forward(request, response);
         request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("username", request.getParameter("username"));
+        //redirecting to JSP
+        //Example: request.getRequestDispatcher("Where my JSP is").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
     }
 
 
